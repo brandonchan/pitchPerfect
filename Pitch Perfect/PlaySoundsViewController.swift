39b507dcc27a1cf18260330f338458236cd06091
Brandon <bchansf@gmail.com>
2015-08-09 12:56:03 -0700

    completed app

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -12,18 +12,28 @@ import AVFoundation
 class PlaySoundsViewController: UIViewController {
     
     var audioPlayer:AVAudioPlayer!
+    var receivedAudio:RecordedAudio!
+    
+    var audioEngine:AVAudioEngine!
+    var audioFile:AVAudioFile!
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3"){
-            var filePathUrl = NSURL.fileURLWithPath(filePath)
-            audioPlayer = AVAudioPlayer(contentsOfURL: filePathUrl, error: nil)
-            audioPlayer.enableRate = true
-        }else {
-            println("the filePath is empty")
-        }
+//        super.viewDidLoad()
+//        if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3"){
+//            var filePathUrl = NSURL.fileURLWithPath(filePath)
+//            audioPlayer = AVAudioPlayer(contentsOfURL: filePathUrl, error: nil)
+//            audioPlayer.enableRate = true
+//        }else {
+//            println("the filePath is empty")
+//        }
+//
+//        // Do any additional setup after loading the view.
+        audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl, error: nil)
+        audioPlayer.enableRate = true
+        
+        audioEngine = AVAudioEngine()
+        audioFile = AVAudioFile(forReading: receivedAudio.filePathUrl, error: nil)
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -46,6 +56,46 @@ class PlaySoundsViewController: UIViewController {
         audioPlayer.play()
     }
     
+    @IBAction func playChipmunkAudio(sender: UIButton) {
+//        FROM ONLINE BUT LINKED LIST IN HOW IT WILL WORK
+//        var audioPlayerNode = AVAudioPlayerNode()
+//        audioEngine.attachNode(audioPlayerNode)
+//        
+//        var changePitchEffect = AVAudioUnitTimePitch()
+//        changePitchEffect.pitch = 1000
+//        audioEngine.attachNode(changePitchEffect)
+//        
+//        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+//        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+//        
+//        audioPlayerNode.play()
+        playAudioWithVariablePitch(1000)
+    }
+   
+    func playAudioWithVariablePitch(pitch: Float){
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        
+        var audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        var changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch
+        audioEngine.attachNode(changePitchEffect)
+        
+        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        audioEngine.startAndReturnError(nil)
+        
+        audioPlayerNode.play()
+    }
+    
+    @IBAction func playDarthvaderAudio(sender: UIButton) {
+        playAudioWithVariablePitch(-1000)
+    }
     
     @IBAction func stopButton(sender: UIButton) {
         audioPlayer.stop()
